71d9f3c95e99141826fc334ab870d3077ec6d56d
Alon Ben Harosh <alonbh5@gmail.com>
2020-06-29 01:42:33 +0300

    WelcomeForm

---
+++ B20 Ex05 Shira 311119002 Alon 312533698/B20_Ex05.FormsUI/WelcomPage.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace B20_Ex05.FormsUI
+{
+    partial class WelcomPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.PVPButton = new System.Windows.Forms.Button();
+            this.BoardSizeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(349, 155);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start!";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Board Size:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Second Player Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "First Player Name:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // PVPButton
+            // 
+            this.PVPButton.Location = new System.Drawing.Point(299, 46);
+            this.PVPButton.Name = "PVPButton";
+            this.PVPButton.Size = new System.Drawing.Size(125, 20);
+            this.PVPButton.TabIndex = 6;
+            this.PVPButton.Text = "Aginst A Friend";
+            this.PVPButton.UseVisualStyleBackColor = true;
+            // 
+            // BoardSizeButton
+            // 
+            this.BoardSizeButton.Location = new System.Drawing.Point(12, 107);
+            this.BoardSizeButton.Name = "BoardSizeButton";
+            this.BoardSizeButton.Size = new System.Drawing.Size(126, 71);
+            this.BoardSizeButton.TabIndex = 7;
+            this.BoardSizeButton.Text = "6 X 6";
+            this.BoardSizeButton.UseVisualStyleBackColor = true;
+            // 
+            // WelcomPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 196);
+            this.Controls.Add(this.BoardSizeButton);
+            this.Controls.Add(this.PVPButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "WelcomPage";
+            this.Text = "WelcomPage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button PVPButton;
+        private System.Windows.Forms.Button BoardSizeButton;
+    }
+}
